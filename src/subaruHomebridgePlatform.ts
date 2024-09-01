8589b614c3e803e389aa714fa727fddfa039301c
Russell Stephens <russell.qwerty@gmail.com>
2024-08-31 21:16:43 -0400

    gut

--- src/subaruHomebridgePlatform.ts
+++ src/subaruHomebridgePlatform.ts
@@ -1,7 +1,7 @@
-import type { API, Characteristic, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
+import { API, Characteristic, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
 
-import { SubaruPlatformLockAccessory } from './platformAccessory.js';
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
+// import { SubaruPlatformLockAccessory } from './subaruPlatformLockAccessory.js';
+// import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 
 interface SubaruHomebridgePlatformConfig extends PlatformConfig {
   username?: string;
@@ -84,62 +84,58 @@ export class SubaruHomebridgePlatform implements DynamicPlatformPlugin {
     // EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+    // const exampleDevices = [
+    //   {
+    //     exampleUniqueId: '123',
+    //     exampleDisplayName: 'Lock',
+    //   },
+    // ];
 
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+    // for (const device of exampleDevices) {
+    //   // generate a unique id for the accessory this should be generated from
+    //   // something globally unique, but constant, for example, the device serial
+    //   // number or MAC address
+    //   const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+    //   // see if an accessory with the same uuid has already been registered and restored from
+    //   // the cached devices we stored in the `configureAccessory` method above
+    //   const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+    //   if (existingAccessory) {
+    //     // the accessory already exists
+    //     this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+    //     // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
+    //     // existingAccessory.context.device = device;
+    //     // this.api.updatePlatformAccessories([existingAccessory]);
 
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new SubaruPlatformLockAccessory(this, existingAccessory);
+    //     // create the accessory handler for the restored accessory
+    //     // this is imported from `platformAccessory.ts`
+    //     new SubaruPlatformLockAccessory(this, existingAccessory, this.log);
 
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+    //     // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
+    //     // remove platform accessories when no longer present
+    //     // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+    //     // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+    //   } else {
+    //     // the accessory does not yet exist, so we need to create it
+    //     this.log.info('Adding new accessory:', device.exampleDisplayName);
 
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+    //     // create a new accessory
+    //     const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
 
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
+    //     // store a copy of the device object in the `accessory.context`
+    //     // the `context` property can be used to store any data about the accessory you may need
+    //     accessory.context.device = device;
 
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new SubaruPlatformLockAccessory(this, accessory);
+    //     // create the accessory handler for the newly create accessory
+    //     // this is imported from `platformAccessory.ts`
+    //     new SubaruPlatformLockAccessory(this, accessory, this.log);
 
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
-    }
+    //     // link the accessory to your platform
+    //     this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    //   }
+    // }
   }
 }
--- src/subaruPlatformLockAccessory.ts
+++ src/subaruPlatformLockAccessory.ts
@@ -1,4 +1,4 @@
-import type { CharacteristicValue, PlatformAccessory, Service } from 'homebridge';
+import type { CharacteristicValue, PlatformAccessory, Service, Logging } from 'homebridge';
 
 import type { SubaruHomebridgePlatform } from './subaruHomebridgePlatform.js';
 
@@ -13,6 +13,7 @@ export class SubaruPlatformLockAccessory {
   constructor(
     private readonly platform: SubaruHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
+    public readonly log: Logging,
   ) {
     // set accessory information
     // TODO
@@ -21,55 +22,47 @@ export class SubaruPlatformLockAccessory {
       .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
       .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
 
-    // get the LockMechanism service if it exists, otherwise create a new LockMechanism service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.LockMechanism) || this.accessory.addService(this.platform.Service.LockMechanism);
+    // create a new Lock Mechanism service
+    this.service = new this.platform.Service('Default-LockMechanism', '');
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
-
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the lockCurrentState/Off Characteristic
+    // create handlers for required characteristics
     this.service.getCharacteristic(this.platform.Characteristic.LockCurrentState)
-      .onSet(this.setLockCurrentState.bind(this)) // SET - bind to the `setLockCurrentState` method below
-      .onGet(this.getLockCurrentState.bind(this)); // GET - bind to the `getLockCurrentState` method below
+      .onGet(this.handleLockCurrentStateGet.bind(this));
+
+    this.service.getCharacteristic(this.platform.Characteristic.LockTargetState)
+      .onGet(this.handleLockTargetStateGet.bind(this))
+      .onSet(this.handleLockTargetStateSet.bind(this));
   }
 
   /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setLockCurrentState(value: CharacteristicValue) {
-    this.service.updateCharacteristic(this.platform.Characteristic.LockCurrentState, value);
+ * Handle requests to get the current value of the "Lock Current State" characteristic
+ */
+  handleLockCurrentStateGet() {
+    this.log.debug('Triggered GET LockCurrentState');
 
-    this.platform.log.debug('Set Characteristic lockCurrentState ->', value);
+    // set this to a valid value for LockCurrentState
+    const currentValue = this.platform.Characteristic.LockCurrentState.UNSECURED;
+
+    return currentValue;
+  }
+
+
+  /**
+ * Handle requests to get the current value of the "Lock Target State" characteristic
+ */
+  handleLockTargetStateGet() {
+    this.log.debug('Triggered GET LockTargetState');
+
+    // set this to a valid value for LockTargetState
+    const currentValue = this.platform.Characteristic.LockTargetState.UNSECURED;
+
+    return currentValue;
   }
 
   /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possible. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.lockCurrentState, true)
-   */
-  async getLockCurrentState(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const lockCurrentState = this.platform.Characteristic.LockCurrentState.UNKNOWN;
-
-    this.platform.log.debug('Get Characteristic lockCurrentState ->', lockCurrentState);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return lockCurrentState;
+ * Handle requests to set the "Lock Target State" characteristic
+ */
+  handleLockTargetStateSet(value: CharacteristicValue) {
+    this.log.debug('Triggered SET LockTargetState:', value);
   }
 }
