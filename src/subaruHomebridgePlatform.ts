3e8110fce75cb1aefc8df3193266ddd5756b1fe4
Russell Stephens <russell.qwerty@gmail.com>
2024-09-26 16:53:35 -0400

    wire up single car lock

--- src/subaruHomebridgePlatform.ts
+++ src/subaruHomebridgePlatform.ts
@@ -4,6 +4,7 @@ import { SubaruPlatformLockAccessory } from './subaruPlatformLockAccessory.js';
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 import { SubaruHomebridgePlatformConfig } from './SubaruHomebridgePlatformConfig.js';
 import { SubaruAPI } from './subaruAPI.js';
+import { Md5 } from 'ts-md5';
 
 /**
  * HomebridgePlatform
@@ -78,65 +79,31 @@ export class SubaruHomebridgePlatform implements DynamicPlatformPlugin {
    * must not be registered again to prevent "duplicate UUID" errors.
    */
   discoverDevices() {
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+    const uuid = this.serviceUUID();
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+    const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+    const deviceName = 'Subaru Door Lock';
 
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+    if (existingAccessory) {
+      this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+      this.api.updatePlatformAccessories([existingAccessory]);
 
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new SubaruPlatformLockAccessory(this, existingAccessory, this.subaruAPI);
+      new SubaruPlatformLockAccessory(this, existingAccessory, this.subaruAPI);
+    } else {
+      this.log.info('Adding new accessory:', deviceName);
 
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+      const accessory = new this.api.platformAccessory(deviceName, uuid);
 
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+      new SubaruPlatformLockAccessory(this, accessory, this.subaruAPI);
 
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new SubaruPlatformLockAccessory(this, accessory, this.subaruAPI);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
+      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
     }
+  }
+
+  public serviceUUID() {
+    const hash = Md5.hashStr(this.config.username || '' + this.config.deviceId || '' + this.config.lastSelectedVehicleKey || '');
+    return this.api.hap.uuid.generate(hash);
   }
 }
