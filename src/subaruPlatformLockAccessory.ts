88e813c086ccc399ce58705608e564e8efd5992a
Russell Stephens <russell.qwerty@gmail.com>
2024-09-29 16:41:20 -0400

    use local value, add logs

--- src/subaruPlatformLockAccessory.ts
+++ src/subaruPlatformLockAccessory.ts
@@ -84,8 +84,10 @@ export class SubaruPlatformLockAccessory {
       this.platform.log.debug('Ignoring handleLockTargetStateSet. isLoading: ', this.isLoading);
       return;
     }
-    if (this.lockCurrentState?.valueOf() === this.lockTargetState?.valueOf()) {
+    if (this.lockCurrentState?.valueOf() === value.valueOf()) {
       this.platform.log.debug('Target state is already current state, ignoring.');
+      this.platform.log.debug('this.lockCurrentState: ', this.lockCurrentState);
+      this.platform.log.debug('handleLockTargetStateSet(value): ', value);
       return;
     }
     this.platform.log.debug('Triggered SET LockTargetState:', value);
